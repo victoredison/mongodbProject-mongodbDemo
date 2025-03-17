7e7ff8604659e2ed50c2d9f141c5a2f25624c817
victoryu <victorwv98@gmail.com>
2025-03-17 15:55:50 +0800

    a little bit update of the comment

--- src/main/java/com/victoryu/mongodbdemo/MongodbDemoApplication.java
+++ src/main/java/com/victoryu/mongodbdemo/MongodbDemoApplication.java
@@ -4,7 +4,7 @@ import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
 
 /**
- * A MongoDB demo
+ * A MongoDB demo exercise
  */
 @SpringBootApplication
 public class MongodbDemoApplication {
